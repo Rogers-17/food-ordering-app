83581ed1ef596da408a2b56a8c5a302f593a4de8
Rogers <mohammedrogers579@gmail.com>
2025-11-16 12:03:04 -0800

    setup appwrite for databases and authentication

---
+++ libs/appwrite.ts
@@ -0,0 +1,5 @@
+export const appwriteConfig = {
+    endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT, // Your Appwrite Endpoint    
+    platform:  "com.jsm.foodordering",
+    projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID, // Your Appwrite Project ID
+};
