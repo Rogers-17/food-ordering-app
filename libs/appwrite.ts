4b2b8b25aca8b515c00cd2eca8e15ebeac422bee
Rogers <mohammedrogers579@gmail.com>
2025-11-16 12:25:12 -0800

    added appwrite USERS table configuration

--- libs/appwrite.ts
+++ libs/appwrite.ts
@@ -1,5 +1,7 @@
 export const appwriteConfig = {
     endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT, // Your Appwrite Endpoint    
     platform:  "com.jsm.foodordering",
-    projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID, // Your Appwrite Project ID
+    projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+    databaseId: '691a2ea0000b83a32fab',
+    userCollectionId: 'user'
 };
