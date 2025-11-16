bfae54118e93138f127227346469da292bc81408
Rogers <mohammedrogers579@gmail.com>
2025-11-16 12:52:37 -0800

    created button and input components and added global types

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,3 +1,5 @@
+import CustomButton from '@/components/CustomButton'
+import CustomInput from '@/components/CustomInput'
 import { images } from '@/constants'
 import { Slot } from 'expo-router'
 import { Dimensions, Image, ImageBackground, KeyboardAvoidingView, Platform, ScrollView, View } from 'react-native'
@@ -13,6 +15,16 @@ const Layout = () => {
             <Image source={images.logo} 
             className='self-center size-48 absolute -bottom-16 z-10'/>
         </View>
+
+        <CustomInput  
+        placeholder='Enter your email'
+        value={''}
+        onChangeText={(text) => {}}
+        label='Email'
+        keyboardType='email-address'
+        />
+        <CustomButton />
+
       </ScrollView>
       <Slot />
     </KeyboardAvoidingView>
